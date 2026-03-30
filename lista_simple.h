c954b5c5411cef44687127c7e90e0f1beae372b6
unknown <ypollo20@gmail.com>
2026-03-30 16:22:15 -0600

    Subo lista_simple.h

--- lista_simple.h
+++ lista_simple.h
@@ -21,39 +21,39 @@ typedef void (*ImprimirFunc)(void*);
 //Funciones para Nodo
 Nodo* crearNodo(void *dato, size_t size);
 void liberarNodo(Nodo *n);
-bool modificarNodo(Nodo *n, void *dato);
+bool modificarNodo(Nodo *n, void *dato,  size_t size);
 
 // Crear / estado
 Lista* crearLista();
 bool esVacia(Lista* lista);
 
 // Inserciones
-void insertarInicio(Lista* lista, void* dato, size_t size);
-void insertarFinal(Lista* lista, void* dato, size_t size);
-void insertarPorPosicion(ista* lista, void* dato, size_t size, int p);
+void insertarInicio(Lista* l, void* dato, size_t size);
+void insertarFinal(Lista* l, void* dato, size_t size);
+void insertarPorPosicion(Lista* l, void* dato, size_t size, int p);
 
 // Eliminaciones
-void eliminarInicio(Lista *lista);
-void eliminarFinal(Lista *lista);
-void eliminarPorPosicion(Lista* lista, int p);
-void eliminarPorElemento(Lista* lista, void* dato, CompararFunc cmp);
-void eliminarElementosIguales(Lista *lista, void *dato, CompararFunc cmp);
+void eliminarInicio(Lista *l);
+void eliminarFinal(Lista *l);
+void eliminarPorPosicion(Lista* l, int p);
+void eliminarPorElemento(Lista* l, void* dato, CompararFunc cmp);
+void eliminarElementosIguales(Lista *l, void *dato, CompararFunc cmp);
 
 // Búsquedas
-void* buscarPorPosicion(Lista* lista, int pos);
-int buscarPorElemento(Lista* lista, void* dato, CompararFunc cmp);
+void* buscarPorPosicion(Lista* l, int pos);
+int buscarPorElemento(Lista* l, void* dato, CompararFunc cmp);
 
 // Navegación
-Nodo* primero(Lista* lista);
-Nodo* ultimo(Lista* lista);
-Nodo* siguiente(Nodo* actual);
+void* primero(Lista* l);
+void* ultimo(Lista* l);
+void* siguiente(void* actual);
 
 // Modificar
-void modificar(Lista* lista, int pos, void* dato, size_t size);
+void modificar(Lista* l, int p, void* dato, size_t size);
 
 // Utilidad
-void imprimirLista(Lista* lista, ImprimirFunc imprimir);
-void vaciarLista(Lista* lista);
-void borrarLista(Lista* lista);
+void imprimirLista(Lista* l, ImprimirFunc imprimir);
+void vaciarLista(Lista* l);
+void borrarLista(Lista* l);
 
 #endif
